0649df5a9d796c9aa4658f5b24339ccbeb478ec6
rodrigoapolo <rodrigoapolodev@gmail.com>
2024-03-28 13:47:53 -0300

    tela de perfil ok

--- agenda/models.py
+++ agenda/models.py
@@ -28,9 +28,7 @@ class Empresa(models.Model):
     id = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=200, blank=True, null=True)
     foto = models.CharField(max_length=200, blank=True, null=True)
-    cep = models.CharField(max_length=200, blank=True, null=True)
-    nome = models.CharField(max_length=200, blank=True, null=True)
-    
+        
 class Servico(models.Model):
     id = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=200, blank=True, null=True)
@@ -38,12 +36,19 @@ class Servico(models.Model):
     valor = models.FloatField()
     tempoSevico = models.TimeField()
     intervalo = models.TimeField()
-    empresa = models.ForeignKey(Empresa, on_delete=models.SET_NULL, blank=True, null=True)
     
 class ServicoFuncionario(models.Model):
     id = models.AutoField(primary_key=True)
     funcionario = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
     servico = models.ForeignKey(Servico, on_delete=models.SET_NULL, blank=True, null=True)
+    
+class EmpresaServico(models.Model):
+    id = models.AutoField(primary_key=True)
+    empresa = models.ForeignKey(Empresa, on_delete=models.SET_NULL, blank=True, null=True)
+    servico = models.ForeignKey(Servico, on_delete=models.SET_NULL, blank=True, null=True)
+    cep = models.CharField(max_length=200, blank=True, null=True)
+    numero = models.CharField(max_length=200, blank=True, null=True)
+    complemento = models.CharField(max_length=200, blank=True, null=True)
         
 class Agenda(models.Model):
     STATUS = (('M', 'Marcado'),('P', 'Programado'), ('E', 'Executado'), ('C', 'Cancelado'))
--- agenda/views.py
+++ agenda/views.py
@@ -2,9 +2,10 @@ import imp
 from re import T
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, update_session_auth_hash
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
+
 
 from agenda import models
 
@@ -23,17 +24,16 @@ def login_in(request):
                 login(request, user)
                 # print(user.pk)
                 # print(user.tipo)
-                return redirect('agenda:index')
+                return redirect('agenda:home')
     else:
         form = AuthenticationForm()
     return render(request, 'agenda/login.html', {'form': form})
 
-@login_required(login_url='agenda:index')
 def logout_view(request):
     auth.logout(request)
-    return redirect('agenda:index')
+    return redirect('agenda:login')
 
-@login_required(login_url='agenda:index')
+@login_required(login_url='agenda:login')
 def home(request):
     return render(request, 'agenda/home.html')
 
@@ -76,13 +76,42 @@ def create_cliente(request):
             return redirect('agenda:home')
         
         return render(request, 'agenda/create-cliente.html', {
-            'usernameErros': usernameErros, 'emailErros': emailErros, 'passwordErros': passwordErros,
-            'username': username, 'email': email, 'password': password, 'confirmar_senha': confirmar_senha
-            })
-        
+                'usernameErros': usernameErros, 'emailErros': emailErros, 'passwordErros': passwordErros,
+                'username': username, 'email': email, 'password': password, 'confirmar_senha': confirmar_senha
+                })
+                    
     else:
         return render(request, 'agenda/create-cliente.html')
 
-
+@login_required(login_url='agenda:login')
 def perfil(request):
-    return render(request, 'agenda/perfil.html')
+    if request.method == 'POST':
+        print(request.POST)
+        username = request.POST['username'] 
+        email = request.POST['email']
+        password = request.POST['password']
+        confirmar_senha = request.POST['confirmar_senha']
+        
+        status = True
+        passwordErros = []
+        user = models.User.objects.get(pk=request.user.pk)
+        
+        if username != '':
+            user.username = username
+        
+        if password != '':
+            if password != confirmar_senha:
+                status = False
+                passwordErros.append('As senhas não conferem.') 
+            else:
+                user.set_password(password)
+        
+        if(status):
+            user.save()
+            return redirect('agenda:logout_view')
+            
+        
+        return render(request, 'agenda/perfil.html', {'passwordErros': passwordErros, 'user': request.user})
+                    
+    else:
+        return render(request, 'agenda/perfil.html', {'user': request.user})
